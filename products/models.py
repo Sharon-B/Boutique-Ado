ad45cea683b1ffbb4eda985515f049d451e27648
Sharon-B <sharonbagnall@gmail.com>
2021-05-25 11:16:42 +0000

    Add search functionality

--- products/models.py
+++ products/models.py
@@ -19,20 +19,23 @@ class Category(models.Model):
 
 class Product(models.Model):
 
-    # category field is a foreign key to the category model, allow it to be null in the database and blank in forms
-    # and if a category is deleted set any products that use it to have null for this field
+    # category field is a foreign key to the category model, allow it to be
+    # null in the database and blank in forms and if a category is deleted
+    # set any products that use it to have null for this field
     # rather than deleting the product.
 
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-    # each product requires a name, description & price all other fields are optional
+    # each product requires name, description & price, others are optional
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -9,12 +11,32 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()        # return all products
+    # start with query set to none:
+    query = None
 
-    context = {             # add our products to the context so they will be available in the template
-        'products': products
+    # Search:
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            # if the query is blank
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            # if the query is not blank
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    # add our products to the context so they will be available in the template
+    # add query to the context for search functionality
+    context = {
+        'products': products,
+        'search_term': query
     }
 
-    return render(request, 'products/products.html', context)   # needs context as we'll need to send some things back to the template
+    return render(request, 'products/products.html', context)
+    # needs context as we'll need to send some things back to the template
 
 
 # Single Product Details view
