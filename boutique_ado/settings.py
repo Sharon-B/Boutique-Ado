cd06713434ef322fe0134540fcc1c39e41f1cd85
Sharon-B <sharonbagnall@gmail.com>
2021-06-21 22:44:05 +0000

    Test static files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -174,6 +174,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -181,7 +182,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS Settings:
 if 'USE-AWS' in os.environ:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-sb'
     AWS_S3_REGION_NAME = 'eu-west-1'
