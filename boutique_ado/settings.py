225476c0306ac8b1697c5487ec55cc550cdd10a2
Sharon-B <sharonbagnall@gmail.com>
2021-06-24 13:45:53 +0000

    Update USE_AWS in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,7 +180,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # AWS Settings:
-if 'USE-AWS' in os.environ:
+if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
